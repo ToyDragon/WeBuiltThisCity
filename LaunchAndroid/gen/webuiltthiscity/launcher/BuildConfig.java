9a3167f92515915842ddd71cc040610d4c9c6e1b
Matthew Bates <BatesMatthewJ@GMail.com>
2014-01-25 21:10:13 -0500

    test commit

---
+++ LaunchAndroid/gen/webuiltthiscity/launcher/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package webuiltthiscity.launcher;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ WeBuiltThisCityAndroidLibrary/src/webuiltthiscity/GraphicsInterface.java
@@ -0,0 +1,12 @@
+package webuiltthiscity;
+
+
+public interface GraphicsInterface {
+	public void drawImage(String image_name, int top_left_x,int top_left_y);
+	public void drawImage(String image_name, int top_left_x,int top_left_y,int width,int height);
+	public void translate(int dx,int dy);
+	public void fill(int fill_color);
+	public void drawText(String text,int top_left_x,int top_left_y);
+	public int[] getDrawAreaDimensions();
+	public void publish();
+}
