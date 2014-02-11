40e30ab3ac2a0c11742ea21690b9ee040ff97cb0
= <=>
2014-02-11 10:27:41 -0500

    Distribute Resources bat now creates missing directories

--- DistributeResources.bat
+++ DistributeResources.bat
@@ -2,18 +2,27 @@
 echo -------
 echo Android
 echo -------
+mkdir "LaunchAndroid/res/drawable-mdpi"
+mkdir "LaunchAndroid/assets/Sounds"
+mkdir "LaunchAndroid/assets/Other"
 xcopy /s/e/y "Resources/Images" "LaunchAndroid/res/drawable-mdpi"
 xcopy /s/e/y "Resources/Sounds" "LaunchAndroid/assets/Sounds"
 xcopy /s/e/y "Resources/Other" "LaunchAndroid/assets/Other"
 echo ----------
 echo NonAndroid
 echo ----------
+mkdir "LaunchNonAndroid/Resources/Images"
+mkdir "LaunchNonAndroid/Resources/Sounds"
+mkdir "LaunchNonAndroid/Resources/Other"
 xcopy /s/e/y "Resources/Images" "LaunchNonAndroid/Resources/Images"
 xcopy /s/e/y "Resources/Sounds" "LaunchNonAndroid/Resources/Sounds"
 xcopy /s/e/y "Resources/Other" "LaunchNonAndroid/Resources/Other"
 echo ----
 echo OUYA
 echo ----
+mkdir "LaunchOUYA/res/drawable-mdpi"
+mkdir "LaunchOUYA/assets/Sounds"
+mkdir "LaunchOUYA/assets/Other"
 xcopy /s/e/y "Resources/Images" "LaunchOUYA/res/drawable-mdpi"
 xcopy /s/e/y "Resources/Sounds" "LaunchOUYA/assets/Sounds"
 xcopy /s/e/y "Resources/Other" "LaunchOUYA/assets/Other"
